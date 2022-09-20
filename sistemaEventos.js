2a80789eb330be6e44f9228d6628dd026cf50fdb
Ferdinan Sobral <ferdinandjr20202>
2022-09-20 20:50:51 -0300

    foi criado o sistema de evento

---
+++ sistemaEventos.js
@@ -0,0 +1,13 @@
+let dataAtual = new Date ();
+let dataEvento= new Date(2023,0,30)
+let idadeparticipante = 15
+let listaparticipantes = ["José","alexandre","Douglas"]
+if (dataEvento>dataAtual) {console.log("agendar evento")
+     } else{ console.log("data invalida")
+}
+
+if(idadeparticipante>=18){console,log("entrada permitida")
+ }else{console.log("idade invalida")}
+  if(listaparticipantes>100){console.log("não ha vagas")
+ }else{console.log(" acesso permitido")
+}
